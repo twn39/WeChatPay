2d07a9e526cd9d958e6bec67b98b7968ad0a2ef9
twn39 <twn39@163.com>
2015-12-14 18:38:53 +0800

    update

--- src/Notify.php
+++ src/Notify.php
@@ -11,28 +11,9 @@ class Notify
      */
     public static function parseXML($xml)
     {
-        $dataObj = simplexml_load_string($xml);
-
-        $data = [];
-
-        $data['appid'] = (string) $dataObj->appid;
-        $data['bank_type'] = (string) $dataObj->bank_type;
-        $cashFee = (string) $dataObj->cash_fee;
-        $data['cash_fee'] = $cashFee / 100;
-        $data['fee_type'] = (string) $dataObj->fee_type;
-        $data['is_subscribe'] = (string) $dataObj->is_subscribe;
-        $data['mch_id'] = (string) $dataObj->mch_id;
-        $data['nonce_str'] = (string) $dataObj->nonce_str;
-        $data['openid'] = (string) $dataObj->openid;
-        $data['out_trade_no'] = (string) $dataObj->out_trade_no;
-        $data['result_code'] = (string) $dataObj->result_code;
-        $data['return_code'] = (string) $dataObj->return_code;
-        $data['sign'] = (string) $dataObj->sign;
-        $data['time_end'] = (string) $dataObj->time_end;
-        $totalFee = (string) $dataObj->total_fee;
-        $data['total_fee'] = $totalFee / 100;
-        $data['trade_type'] = (string) $dataObj->trade_type;
-        $data['transaction_id'] = (string) $dataObj->transaction_id;
+        $data = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+        $data['cash_fee'] = $data['cash_fee'] / 100;
+        $data['total_fee'] = $data['total_fee'] / 100;
 
         return $data;
     }
